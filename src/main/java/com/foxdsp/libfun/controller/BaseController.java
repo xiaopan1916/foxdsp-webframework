df49a50decc436aab25d448f8e3a4832d00445ba
panfenquan <panfenquan@foxdsp.com>
2018-06-23 15:43:19 +0800

    feature1

--- src/main/java/com/foxdsp/libfun/controller/BaseController.java
+++ src/main/java/com/foxdsp/libfun/controller/BaseController.java
@@ -144,7 +144,7 @@ public class BaseController extends Controller {
         }
         for (int i = 0; i <100; i++) {
             System.out.println("什么鬼");
-            System.out.println("这个是dev");
+            System.out.println("这个xxxxxxxxx是dev");
         }
         UserInfo info = (UserInfo) userInfo;
         return info.getUserlogno();
